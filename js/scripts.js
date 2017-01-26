596ae8af4216fd37443b88ce6fc43be348aa68e6
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-01-25 23:41:38 -0800

    add Block constructor, generateBlock func, insertBlock func, blockCondition func, and running program

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,44 @@
 $(function(){
 
+  //object
+  function Block(topLeft, topRight, bottomRight, bottomLeft, blockId){
+      this.topLeft = topLeft;   //[x, y]
+      this.topRight = topRight; //[x, y]
+      this.bottomRight = bottomRight; //[x, y]
+      this.bottomLeft = bottomLeft;  //[x, y]
+      this.blockId = blockId;
+      this.width = topRight[0] - topLeft[0];
+      this.height = bottomLeft[1] - topLeft[1];
+      this.color = "blue";
+      };
+
+  var blockId = 0;
+  var newBlock = new Block( [0, 0], [10, 0], [10, 10], [0, 10], blockId)
+  var newBlock2 = new Block( [100, 100], [150, 100], [150, 150], [100, 150], blockId);
+  var newBlock3 = new Block( [220, 100], [430, 100], [290, 220], [220, 320], blockId);
+  var generateBlock = function(newBlock){
+    $("#newBlock" + blockId).css("position", "absolute");
+    $("#newBlock" + blockId).css("left", newBlock.topLeft[0] + "px");
+    $("#newBlock" + blockId).css("top", newBlock.topLeft[1] + "px");
+    $("#newBlock" + blockId).css("height", newBlock.height + "px");
+    $("#newBlock" + blockId).css("width", newBlock.width + "px");
+    $("#newBlock" + blockId).css("background-color", newBlock.color);
+  }
+  var insertBlock = function(newBlock){
+    $("#generate-block-here").append('<div id=' + '"' + 'newBlock' + blockId + '"' +  '</div>');
+    generateBlock(newBlock);
+    blockId++;
+  }
+
+  var blockCondition = function(newBlock){
+   return (left > newBlock.topLeft[0] && left < (newBlock.topLeft[0] + newBlock.width)) && (top > newBlock.topLeft[1] && top < (newBlock.topLeft[1] + newBlock.height))
+  }
+
+  insertBlock(newBlock);
+  insertBlock(newBlock2);
+  insertBlock(newBlock3);
+
+
   ///////////////////////////////////////////////
   ////////////// Move Function!!/////////////////
   //////////////////////////////////////////////
@@ -38,25 +77,24 @@ $(function(){
       if (keysPressed[3])
           top += speed;
 
-      $("#moveDiv").css("left", left + "px");
-      $("#moveDiv").css("top", top + "px");
-
+      $("#newBlock0").css("left", left + "px");
+      $("#newBlock0").css("top", top + "px");
 
 
       currentPosition = [left, top]
       console.log(currentPosition);
 
+
       //insert comment based on the current position
       if(left < 0 || left > 600 || top > 600 || top < 0){
         $("#comment").text("You are out of border!");
-      }else if( (left > 500 && top > 300) && (left < 600 && top < 400)){
-        $("#comment").text("You hit blue box!!");
-
+      }else if( blockCondition(newBlock2)){
+        $("#comment").text("You hit the small blue box!!");
+      }else if( blockCondition(newBlock3)){
+        $("#comment").text("You hit the big blue box!!");
       }else{
         $("#comment").text("You are inside of border!");
       }
-
-
 
       if (spacePressed)
           $("#spacePressedShow").css("display", "block");
